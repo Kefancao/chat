4a79113b647e73c79fc1fe267b80c7458541721d
Kefan Cao <k33cao@uwaterloo.ca>
2021-08-17 15:00:49 -0400

    Added message sender

--- src/Chat.js
+++ src/Chat.js
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Message from './Message'
 
 export default function Chat() {
+	const [message, setMessage] = useState(''); 
+
+	const updateMessage = (event) => {
+		setMessage(event.target.value)
+	}
+
+	const sendMessage = () => {
+		// Send Message into FireBase
+	}
 	return (
 		<>
-			<Message />
+			<div className="messageBox">
+				<Message />
+			</div>
+			<div className="sender">
+				<form>
+					<input type="text" placeholder="Type here..." onChange={event => updateMessage(event)}></input>
+					<button onClick={sendMessage}>Send</button>
+				</form>
+			</div>
 		</>
 	)
 }
